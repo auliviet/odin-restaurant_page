0ae6c3e190cb305987eca9d669c972daf5a251f9
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-06-29 08:45:52 +1000

    Add functions to manage the tabbed navigation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,33 @@
+// Stylesheets
 import "./style.css"
+
+// Pages
 import home from "./pages/home/";
+import about from "./pages/about/";
+
+// Event listeners
+let buttons = document.querySelectorAll("button");
+
+for (let button of buttons) {
+    let id = button.id;
+
+    switch (id) {
+        case "home":
+            button.addEventListener("click", home);
+            break;
+        
+        case "menu":
+            button.addEventListener("click", menu);
+            break;
+        
+        case "about":
+            button.addEventListener("click", about);
+            break;
+    
+        default:
+            break;
+    }
+}
+
 
 home();
