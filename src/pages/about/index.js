356c8f2e11fea589ab17ab984cf53bbe9ef7ccce
Auliviet <112747495+auliviet@users.noreply.github.com>
2024-06-30 14:41:03 +1000

    Create content for about page

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,11 +1,29 @@
 import "./index.css";
 
+import mrKrab from "./mrKrab.png"
+
 export default function about() {
     let container = document.querySelector("#content");
     container.textContent = "";
+
+    let mrKrabImg = new Image();
+    mrKrabImg.src = mrKrab;
+    container.append(mrKrabImg);
+
+    let headings = document.createElement("div");
+    headings.className = "about__headings";
     
     const title = document.createElement("h1");
-    title.textContent = "About";
-    container.append(title);
+    title.textContent = "The Krusty Krab";
+    headings.append(title);
 
+    const subTitle = document.createElement("h2");
+    subTitle.textContent = "Come spend your money here!";
+    headings.append(subTitle);
+
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "The Krusty Krab is a fast food restaurant founded and owned by Eugene H. Krabs. It is also the most popular and best known restaurant in Bikini Bottom. \n Famous for its Krabby Patty burgers, with its secret recipe."
+    headings.append(paragraph);
+
+    container.append(headings);
 }
